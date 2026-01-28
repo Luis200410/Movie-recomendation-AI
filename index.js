8e11752beb81c8c68bb86d5a8b57f96e5b1206d3
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-01-28 13:42:22 -0500

    created the embeds for each movie

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import { openai, supabase } from './config.js';
+import data from './content.js'
+
+async function createAndStoreEmbeds(data){
+    const dataReturned = await Promise.all(
+        data.map(async (movie) => {
+            const embeddingResponse = await openai.embeddings.create({
+            model: "text-embedding-3-small",
+            input: movie.content,
+            });
+            return {
+                content: movie.content,
+                embedding: embeddingResponse.data[0].embedding
+            }
+        })
+    )
+    await supabase.from('movie_project').insert(data)
+}
+
