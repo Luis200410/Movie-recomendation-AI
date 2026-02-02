97bccd5823a8963d8908261e8eba7311647aa2aa
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-02-02 11:05:19 -0500

    updated token

--- config.js
+++ config.js
@@ -16,3 +16,7 @@ if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
 const url = import.meta.env.VITE_SUPABASE_URL;
 if (!url) throw new Error(`Expected env var SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
+
+// movies API config
+
+export const moviesAPI = import.meta.env.VITE_TMDB_TOKEN;
